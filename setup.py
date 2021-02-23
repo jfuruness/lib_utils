c825233e0086b4fe6538a2619232d35978817cff
Justin Furuness <jfuruness@gmail.com>
2021-02-23 03:03:21 -0500

    fixed versioning

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 setup(
     name='lib_utils',
     packages=find_packages(),
-    version='0.1.5',
+    version='0.1.6',
     author='Justin Furuness',
     author_email='jfuruness@gmail.com',
     url='https://github.com/jfuruness/lib_utils.git',

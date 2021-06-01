82111d0bcabc267eb2f17494dc725ae4b09d52d4
Justin Furuness <jfuruness@gmail.com>
2021-06-01 03:56:50 -0400

    Added file handler to logging in lib_utils

--- lib_utils/print_funcs.py
+++ lib_utils/print_funcs.py
@@ -1,29 +1,49 @@
 """Contains useful functions relating to printing"""
 
+from datetime import datetime
 import functools
 import logging
+import os
 import sys
 
 import multiprocessing_logging
 
+from .file_funcs import makedirs
 
-def config_logging(level=logging.INFO):
+
+def config_logging(level=logging.INFO, section="main"):
     """Configures logging to log to a file and screen"""
 
-    if len(logging.root.handlers) != 1:
+    if len(logging.root.handlers) == 0:
+
+        path = _get_log_path(section)
+
         logging.root.handlers = []
         logging.basicConfig(level=level,
                             format='%(asctime)s-%(levelname)s: %(message)s',
-                            handlers=[logging.StreamHandler()])
+                            handlers=[logging.StreamHandler(),
+                                      logging.FileHandler(path)])
 
         logging.captureWarnings(True)
         multiprocessing_logging.install_mp_handler()
         logging.debug("initialized logger")
 
 
+def _get_log_path(section):
+    """Returns path to log file"""
+
+    fname = f"{section}_{datetime.now().strftime('%Y_%m_%d_%M_%S.%f')}.log"
+    log_dir = f"/var/log/{section}/"
+
+    makedirs(log_dir)
+
+    return os.path.join(log_dir, fname)
+
+
 def write_to_stdout(msg: str):
     sys.stdout.write(f"{msg}\n")
     sys.stdout.flush()
+
 
 # This decorator prints exception upon err
 def print_err(err, msg="{}"):
